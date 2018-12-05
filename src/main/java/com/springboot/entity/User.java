9e00f4055eea3848272de02cbce72f4da4ece432
vino <1658895307@qq.com>
2018-12-06 01:20:37 +0800

    add User Entity

--- src/main/java/com/springboot/entity/User.java
+++ src/main/java/com/springboot/entity/User.java
@@ -1,7 +1,45 @@
 package com.springboot.entity;
 
+import javax.persistence.*;
 import java.io.Serializable;
-
+@Entity
+@Table(name = "user")
 public class User extends BasicInfomation implements Serializable {
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+    @Column(name = "name",length = 100)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Column(name = "eamil",length = 50)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    @Column(name = "phone",length = 100)
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
