fbefade63e1a28ff6354ebe782582a83600d305d
vino <1658895307@qq.com>
2018-12-07 00:25:35 +0800

    add jpa and improve entity

--- src/main/java/com/springboot/controller/UserController.java
+++ src/main/java/com/springboot/controller/UserController.java
@@ -5,13 +5,23 @@ import com.springboot.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class UserController {
     @Autowired
     UserRepository userRepository;
 
+    @ResponseBody
+    @GetMapping("/user/{id}")
+    public List<User> getUser(@PathVariable("id") Long id){
+
+        List<User> users = userRepository.findAll();
+        return users;
+    }
 
     @ResponseBody
     @GetMapping("/user")
