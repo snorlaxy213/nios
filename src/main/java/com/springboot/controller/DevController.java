12e0d7e8466923479073052000575a6f423d2797
vino <1658895307@qq.com>
2018-12-28 23:17:08 +0800

    delete master files

--- src/main/java/com/springboot/controller/DevController.java
+++
@@ -1,8 +0,0 @@
-package com.springboot.controller;
-
-public class DevController {
-    public static void main(String[] args) {
-        System.out.println("123");
-        System.out.println("123");
-    }
-}
--- src/main/java/com/springboot/controller/TestController.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.controller;
-
-public class TestController {
-    public static void main(String[] args) {
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("it s mine");
-    }
-}
